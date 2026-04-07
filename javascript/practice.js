1fdd50b032181e5cc567d31148db73d1af648b10
AnitaPK <anita.khamitkar@gmail.com>
2026-04-07 12:54:38 +0530

    practice

---
+++ javascript/practice.js
@@ -0,0 +1,61 @@
+var a 
+
+console.log(a)
+
+a = 10
+
+
+let b 
+console.log(b)
+z = a+10
+b = 'AAA'
+
+async function fetchData(){
+    await fetch('')
+    .then()
+    .then()
+    .catch()
+}
+
+
+try{
+    fetchData()
+    console.log(data)
+}catch(error){
+    console.log(error)
+    alert("")
+}finally{
+    console("task complete")
+}
+
+emp ={
+    name:"Aaa",
+    city:"Pune",
+    phone:"3456789"
+}
+// spread operator 
+// [...emp, {isLoggin:true}]
+
+emp?.city
+
+
+cond ? true : false
+
+empCity = emp.address.city 
+
+
+
+
+
+const {name,city,phone} = emp
+
+console.log(name)
+
+
+
+arr1 = [1,2,3]
+
+// [...arr1, 4,5]
+
+[1,2,3,4,5]
+
